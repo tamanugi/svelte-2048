5befc471099b4c1a245e1ebab71ac712e28fdbc0
tamanugi <tamanugi@gmail.com>
2021-06-28 16:14:03 +0900

    implement board and cell components

---
+++ src/lib/Board/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Cell from '$lib/Cell/index.svelte';
+
+  export let board: number[];
+</script>
+
+<div class="board">
+  {#each board as cell}
+    <Cell {cell}></Cell>
+  {/each}
+</div>
+
+<style>
+.board {
+  display: inline-grid;
+  grid-template-columns: repeat(4, 64px);
+  row-gap: 10px;
+  column-gap: 10px;
+  background-color: #F5ACC9;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+</style>
---
+++ src/lib/Cell/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+export let cell:number = 0;
+
+function calcHsl(cell: number) {
+
+  if (cell === 0) {
+    return '#CECECE';
+  }
+
+  const n = Math.log2(cell);
+  return `hsl(${50 + (20 * n)}, 72%, 72%)`
+}
+
+</script>
+
+<div class="cell" style="{'background-color: ' + calcHsl(cell)}">
+  {cell > 0 ? cell : ''}
+</div>
+
+<style>
+.cell {
+  display: flex;
+  width: 64px;
+  height: 64px;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 10px;
+  color: white;
+  font-weight: bold;
+  box-shadow: 3px 3px 3px grey;
+}
+</style>
